fba3ed4f9245fdda3bd33597e66290c958e73bdf
edolubis21 <edolubis12345@gmail.com>
2020-12-28 00:00:07 +0700

    detail my post

--- lib/screens/my_profile/widgets/my_post.dart
+++ lib/screens/my_profile/widgets/my_post.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:wisata_aja/blocs/my_postings/my_postings_state.dart';
+import 'package:wisata_aja/widgets/posts/detail_post.dart';
 
 class MyPost extends StatelessWidget {
   final MyPostingsLoaded posts;
@@ -18,12 +20,17 @@ class MyPost extends StatelessWidget {
         physics: NeverScrollableScrollPhysics(),
         children: <Widget>[
           for (var data in posts.getListWithImage)
-            Padding(
-              padding: const EdgeInsets.only(left: 2, bottom: 2, right: 2, top: 2),
-              child: FadeInImage.assetNetwork(
-                placeholder: "assets/grey_placeholder.png",
-                image: data.imageposts[0].urlImg,
-                fit: BoxFit.cover,
+            GestureDetector(
+              onTap: () => Get.to(DetailPost(
+                post: data,
+              )),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 2, bottom: 2, right: 2, top: 2),
+                child: FadeInImage.assetNetwork(
+                  placeholder: "assets/grey_placeholder.png",
+                  image: data.imageposts[0].urlImg,
+                  fit: BoxFit.cover,
+                ),
               ),
             )
         ],
